6445195f3e31e3d3573ed0a69216f4f0627f2434
Slim Aloui <s.aloui@n-3rd.com>
2023-03-08 12:10:09 +0100

    remove unused code

--- app/src/main/java/com/yassir/movies/domain/util/Resource.kt
+++ app/src/main/java/com/yassir/movies/domain/util/Resource.kt
@@ -11,22 +11,4 @@ sealed class Resource<T>(val data: T? = null, val message: String? = null) {
 
 
 
-   /* package com.ngallazzi.domain.common
-
-    *//**
-     * A generic class that holds a value or an error status.
-     * @param <T>
-     *//*
-    sealed class Result<out R> {
-
-        data class Success<out T>(val data: T) : Result<T>()
-        data class Error(val exception: Exception) : Result<Nothing>()
-
-        override fun toString(): String {
-            return when (this) {
-                is Success<*> -> "Success[data=$data]"
-                is Error -> "Error[exception=$exception]"
-            }
-        }
-    }*/
 }
